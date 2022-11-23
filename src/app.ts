421cd27212a5c27babef512cef9d5bb57ea7f874
Roman Kolmakov <smtexx@hotmail.com>
2022-11-23 11:02:27 +0100

    Create function handleClickOnTodo and auxiliary functions

--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,6 @@ async function initApp() {
     }
   }
 }
-
 async function handleFormSubmit(event: SubmitEvent) {
   event.preventDefault();
 
@@ -80,7 +79,6 @@ async function handleFormSubmit(event: SubmitEvent) {
     }
   }
 }
-
 function createTodo(): UnregisteredTodo {
   const newTodo: UnregisteredTodo = {
     userId: 0,
@@ -104,7 +102,6 @@ function createTodo(): UnregisteredTodo {
 
   return newTodo;
 }
-
 async function registerTodo(todo: UnregisteredTodo): Promise<Todo> {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
     method: 'POST',
@@ -120,7 +117,6 @@ async function registerTodo(todo: UnregisteredTodo): Promise<Todo> {
     throw new Error('Unable to register new todo on server');
   }
 }
-
 function renderTodo(todo: Todo): void {
   const li = document.createElement('li');
   li.className = 'todo-item';
@@ -149,10 +145,108 @@ function renderTodo(todo: Todo): void {
     );
   }
 }
-
 function getUserName(userId: number): string {
   const user = usersStore.find((u) => u.id === userId);
   return user ? user.name : 'Unknown';
+}
+async function handleClickOnTodo(event: MouseEvent) {
+  const element = event.target;
+  if (!element || !(element instanceof HTMLElement)) return;
+
+  // Handle change todo status
+  if (
+    element.classList.contains('todo-status') &&
+    element instanceof HTMLInputElement
+  ) {
+    try {
+      const todoId = getParentTodoId(element);
+      await changeTodoStatus(todoId, element.checked);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error);
+        alert('Unable to change Todo status');
+      }
+    }
+  }
+
+  // Handle todo close
+  if (element.classList.contains('todo-close')) {
+    try {
+      const todoId = getParentTodoId(element);
+      await closeTodo(todoId);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error);
+        alert('Unable to delete Todo');
+      }
+    }
+  }
+}
+function getParentTodoId(el: HTMLElement): number {
+  const id = el.parentElement?.dataset.id;
+  const idNumber = Number(id);
+  if (!Number.isNaN(idNumber)) {
+    return idNumber;
+  } else {
+    throw new Error(`Unable to extract number from ID ${id}`);
+  }
+}
+async function changeTodoStatus(todoId: number, status: boolean) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    {
+      method: 'PATCH',
+      body: JSON.stringify({ completed: status }),
+      headers: { 'Content-Type': 'application/json' },
+    }
+  );
+
+  const todo = todosStore.find((t) => t.id === todoId);
+
+  if (todo) {
+    if (response.ok) {
+      const data: unknown = await response.json();
+      todo.completed = status;
+    } else {
+      const inputElement = todoList?.querySelector(
+        `li[data-id=${todoId}] .todo-status`
+      );
+
+      if (inputElement instanceof HTMLInputElement) {
+        inputElement.checked = todo.completed;
+      }
+
+      throw new Error('Unable to synchronize Todo status changing with server');
+    }
+  } else {
+    throw new Error(
+      'Can not find Todo to synchronize status in the todosStore'
+    );
+  }
+}
+async function closeTodo(todoId: number) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  if (response.ok) {
+    deleteTodo(todoId);
+  } else {
+    throw new Error(`Can not delete Todo with ID ${todoId}`);
+  }
+}
+function deleteTodo(todoId: number) {
+  // Delete from todosStore
+  todosStore = todosStore.filter((t) => t.id !== todoId);
+  // Delete from page
+  const todoElement = todoList?.querySelector(`li[data-id=${todoId}]`);
+  todoElement?.remove();
 }
 
 (function () {
